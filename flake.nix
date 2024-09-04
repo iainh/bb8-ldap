76f8ad528d8ef3c3b296821b853e070c2d53533e
Iain H <iain@spiralpoint.org>
2024-09-04 15:59:28 +0000

    Add rust-analyzer to development environment

--- flake.nix
+++ flake.nix
@@ -70,6 +70,7 @@
           # Extra inputs can be added here; cargo and rustc are provided by default.
           packages = [
             # pkgs.ripgrep
+            pkgs.rust-analyzer
           ];
         };
       }
